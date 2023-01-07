8907f3fd82e3d1dd383b5ccd1c1b70f77d4f01b6
PhilJollans <phil@jollans.com>
2023-01-07 09:51:08 +0100

    More updates to test program.
    Add a second AutoCompleteBox using a simple list of string, and no DataTemplate.
    This has exactly the same performance problem as the existing AutoCompleteBox.
    
    Add a stupid logic to populate the list with 100 items for the first query, but
    then 1000 items for all subsequent queries. This is a substantial improvement!

--- WpfToolkit/AutoCompleteTest/ViewModel.cs
+++ WpfToolkit/AutoCompleteTest/ViewModel.cs
@@ -16,6 +16,8 @@ namespace AutoCompleteTest
     private List<WordDefViewModel>    _wordDefList   = null ;
     private WordDefViewModel          _currentWord   = null ;
     private WordDefViewModel          _selectedWord  = null ;
+    private List<String>              _basicWordList = null ;
+    private bool                      _firstPopulation = true ;
 
     public string PopulateText { get; set; }
 
@@ -24,14 +26,25 @@ namespace AutoCompleteTest
     public WordDefViewModel       SelectedWord { get => _selectedWord; set => SetProperty ( ref _selectedWord, value ); }
     public int                    WordCount => WordDefList == null ? 0 : WordDefList.Count ;
 
+    public List<String>           BasicWordList { get => _basicWordList; set => SetProperty ( ref _basicWordList, value ); }
+
     public void Populate()
     {
+      //
       // Using MAXRECORDS = 1000, it takes a long time to show the drop down list for the first time.
       // After that, it fills much more quickly. I cannot figure out why this is, and it appeart to
       // be a WPF effect, and not a problem in the AutoCompleteBox.
       // Using MAXRECORDS = 100, the performance is better, but it requires more round trips to the
       // data source.
-      const int MAXRECORDS = 100 ;
+      //
+      // I have added a second AutoCompleteBox, which binds to a List<string> and does not use a
+      // DateTemplate. This has exactly the same problem, so it is not the DataTemplate which is
+      // causing the problem.
+      //
+      // However, after opening the list in either one of the two AutoCompleteBoxes, opening the list
+      // in the other one is also faster.
+      //
+      const int MAXRECORDS = 1000 ;
 
       if ( ( string.IsNullOrEmpty ( _currentPrefix ) )
            ||
@@ -39,8 +52,15 @@ namespace AutoCompleteTest
            ||
            ( WordDefList.Count >= MAXRECORDS ) )
       {
+        //
+        // I have now found that the performance is good if we use 100 the first time that we
+        // populate the list, but then 1000 for subsequent loads.
+        // In fact, loading 100 and then 1000 is quicker than loading 1000 directly.
+        //
+        int usedMax = _firstPopulation ? 100 : MAXRECORDS ;
+
         string url           = "https://api.datamuse.com" ;
-        string urlParameters = $"words?sp={PopulateText}*&max={MAXRECORDS}";
+        string urlParameters = $"words?sp={PopulateText}*&max={usedMax}";
 
         using ( var client = new HttpClient () )
         {
@@ -58,8 +78,18 @@ namespace AutoCompleteTest
             WordDefList = response.Content.ReadAsAsync<List<WordDefViewModel>> ().Result;
             OnPropertyChanged ( nameof( WordCount ) );
 
-            // Finally store the prefix used to generate the list
-            _currentPrefix = PopulateText;
+            // Also create a simple list of words
+            BasicWordList = WordDefList.Select ( x => x.Word ).ToList ();
+
+            if ( _firstPopulation )
+            {
+              _firstPopulation = false ;
+            }
+            else
+            {
+              // Finally store the prefix used to generate the list
+              _currentPrefix = PopulateText;
+            }
           }
           else
           {
