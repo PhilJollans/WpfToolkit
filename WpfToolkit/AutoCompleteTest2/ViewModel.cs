7beb20f62dea72173763e06e465c25d6594a88a1
PhilJollans <phil@jollans.com>
2023-01-08 15:10:59 +0100

    Another simple test program
    This is another simple test program to demonstrate the performance problem in the AutoCompleteBox, opening the drop down list with 1000 items for the first time.
    The test contains two identical controls, each with a list of 1000 items. The first one you open takes a long time. The second one is fast and all subsequent operations are fast. This illustrates that whatever is causing the performance problem is independent of the  specific control.

---
+++ WpfToolkit/AutoCompleteTest2/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCompleteTest2
+{
+  internal class ViewModel
+  {
+    public List<string> WordList1 { get; private set; }
+    public List<string> WordList2 { get; private set; }
+
+    public ViewModel()
+    {
+      WordList1 = new List<string>();
+      for ( int i = 0 ; i< 1000 ; i++ )
+      {
+        WordList1.Add ( $"A{i}" ) ;
+      }
+
+      WordList2 = new List<string> ();
+      for ( int i = 0 ; i < 1000 ; i++ )
+      {
+        WordList2.Add ( $"B{i}" );
+      }
+    }
+
+  }
+}
